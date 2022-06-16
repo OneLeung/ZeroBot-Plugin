30c95357141df0549742ad3d6bf9c0ed330a4fb2
OneLeung <35070891+OneLeung@users.noreply.github.com>
2022-06-17 02:19:37 +0800

    Update chat.go

--- plugin/chat/chat.go
+++ plugin/chat/chat.go
@@ -8,9 +8,9 @@ import (
 
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
-	//zero "github.com/wdvxdr1123/ZeroBot"
-	//"github.com/wdvxdr1123/ZeroBot/extension/rate"
-	//"github.com/wdvxdr1123/ZeroBot/message"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/extension/rate"
+	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
 var (
